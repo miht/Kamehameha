60160fdeed83e5984064a4babda020c1b70e0de1
BuildTools <unconfigured@null.spigotmc.org>
2018-02-23 08:07:43 +0100

    minor changes

---
+++ vector2d.cpp
@@ -0,0 +1,10 @@
+#include "vector2d.h"
+
+Vector2D::Vector2D(double x, double y)
+    :   x(x), y(y)
+{
+}
+
+Vector2D::Vector2D()
+{
+}
---
+++ vector2d.h
@@ -0,0 +1,15 @@
+#ifndef VECTOR2D_H
+#define VECTOR2D_H
+
+
+class Vector2D
+{
+public:
+    double x, y;
+    Vector2D();
+    Vector2D(double x, double y);
+
+
+};
+
+#endif // VECTOR2D_H
